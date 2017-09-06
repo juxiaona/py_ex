c0bdad9dd166fb72593f765ebd106ac5dc76c0b8
juxiaona <juxiaona@xxkuaipao.com>
2017-09-06 15:05:09 +0800

    modify baidu_setting.py

--- test_case/test_baidu/baidu_setting.py
+++ test_case/test_baidu/baidu_setting.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append('../../')
 from base.base import Base
+from base.HTMLTestRunner import HTMLTestRunner
 import unittest
-from selenium import webdriver
-from time import sleep
+import time
 
 class BaiduSet(unittest.TestCase):
 
@@ -14,6 +14,7 @@ class BaiduSet(unittest.TestCase):
 		self.base.max_window()
 
 	def test_setting(self):
+		'''设置功能'''
 		self.base.move_to_element("link_text=>设置")
 		self.base.click_element("link_text=>搜索设置")
 		self.base.select_element('id=>nr', '50')
@@ -23,5 +24,18 @@ class BaiduSet(unittest.TestCase):
 		self.base.quit()
 
 if __name__ == '__main__':
+
 	unittest.main()
 
+	'''
+	suite=unittest.TestSuite()
+	suite.addTest(BaiduSet('test_setting'))
+
+	now=time.strftime("%Y-%m-%d-%H_%M_")
+
+	filename="../../report/"+now+"report.html"
+	fp=open(filename,"wb")
+	runner=HTMLTestRunner(stream=fp,title="测试报告",description="用例执行情况")
+	runner.run(suite)
+	fp.close()
+	'''
